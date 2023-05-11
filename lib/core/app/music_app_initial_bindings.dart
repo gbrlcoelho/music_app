060f2f03a7ccf7560915078e8fa4e985b788abdb
gccano <gccano@viptech.com.br>
2023-05-11 19:24:21 -0300

    feat(app): add dio base options

--- lib/core/app/music_app_initial_bindings.dart
+++ lib/core/app/music_app_initial_bindings.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:get/get.dart';
+import 'package:music_app/core/services/dot_env_service.dart';
 
 import '../../shared/features/music_player/presentation/controllers/music_player_controller.dart';
 import '../services/api_service.dart';
@@ -7,7 +9,13 @@ import '../services/audio_service/audio_player_service_impl.dart';
 class MusicAppInitialBindings extends Bindings {
   @override
   void dependencies() {
-    Get.put(ApiService());
+    Get.put(ApiService(
+      BaseOptions(
+        baseUrl: DotEnvService.getApiBaseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
+      ),
+    ));
 
     Get.put(AudioPlayerServiceImpl());
 
