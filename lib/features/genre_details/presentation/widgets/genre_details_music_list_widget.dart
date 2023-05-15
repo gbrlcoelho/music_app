47ec1ab06a535c4e431ab78f167a294849491bed
gbrlcoelho <coelhogabriel67@gmail.com>
2023-05-14 22:28:27 -0300

    feat(genre_details_screen.dart): add musics list on genre details screen

--- lib/features/genre_details/presentation/widgets/genre_details_music_list_widget.dart
+++ lib/features/genre_details/presentation/widgets/genre_details_music_list_widget.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:get/instance_manager.dart';
+import 'package:music_app/core/app/music_app_colors.dart';
 import 'package:music_app/features/genre_details/presentation/controllers/genre_details_controller.dart';
 import 'package:music_app/shared/features/music_player/presentation/controllers/music_player_controller.dart';
 import 'package:music_app/shared/features/music_player/presentation/widgets/music_player_controls/widgets/circular_progress_indicator_widget.dart';
 import 'package:music_app/shared/widgets/app_music_error_widget.dart';
+import 'package:music_app/shared/widgets/img_and_title_row_widget.dart';
 import 'package:music_app/shared/widgets/text_widget.dart';
 
+import '../../../../shared/models/music_model.dart';
 import '../bindings/genre_details_bindings.dart';
 
 class GenreDetailsMusicListWidget extends StatelessWidget {
@@ -15,6 +18,14 @@ class GenreDetailsMusicListWidget extends StatelessWidget {
     required this.genreSearchString,
     super.key,
   });
+
+  Color _getMusicTitleColor(
+      MusicModel currentMusicInList, MusicModel? currentPlayingMusic) {
+    if (currentMusicInList.title == currentPlayingMusic?.title) {
+      return MusicAppColors.tertiaryColor;
+    }
+    return MusicAppColors.secondaryColor;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -43,6 +54,41 @@ class GenreDetailsMusicListWidget extends StatelessWidget {
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     TextWidget.title('Músicas'),
+                    const SizedBox(height: 12),
+                    ...genreDetailsController.genreDetails?.musics
+                            .asMap()
+                            .map(
+                              (int index, MusicModel music) => MapEntry(
+                                index,
+                                Builder(
+                                  builder: (builderContext) {
+                                    return Column(
+                                      children: [
+                                        InkWell(
+                                          onTap: () => musicPlayerController
+                                              .playSelectedMusic(
+                                                  context, index),
+                                          child: ImgAndTitleRowWidget(
+                                            heroTag: index.toString(),
+                                            title: music.title,
+                                            img: music.img,
+                                            titleColor: _getMusicTitleColor(
+                                              music,
+                                              musicPlayerController
+                                                  .getCurrentPlayingMusic,
+                                            ),
+                                          ),
+                                        ),
+                                        const SizedBox(height: 12),
+                                      ],
+                                    );
+                                  },
+                                ),
+                              ),
+                            )
+                            .values
+                            .toList() ??
+                        [],
                   ],
                 ),
               ),
