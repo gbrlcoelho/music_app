21e7839e57004d0375b4e81c4682e630c38caf02
gbrlcoelho <coelhogabriel67@gmail.com>
2023-05-15 08:09:55 -0300

    feat(widgets): created music player widget!

---
+++ lib/shared/features/music_app/presentation/widgets/music_player_widget.dart
@@ -0,0 +1,40 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:music_app/core/app/music_app_colors.dart';
+import 'package:music_app/shared/models/music_model.dart';
+
+class MusicPlayerWidget extends StatelessWidget {
+  final MusicModel music;
+
+  const MusicPlayerWidget({required this.music, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BackdropFilter(
+      filter: ImageFilter.blur(
+        sigmaX: 25,
+        sigmaY: 25,
+      ),
+      child: Align(
+        alignment: Alignment.topLeft,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 14),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                IconButton(
+                  onPressed: Get.back,
+                  icon: const Icon(Icons.arrow_downward_outlined),
+                  color: MusicAppColors.secondaryColor,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
